c16f1c4a791de3fbd68832d77b572ff63b8e96c2
Amogh Badikol <bamogh.reddy@gmail.com>
2026-09-08 10:40:55 -0400

    Initial Commit

---
+++ BiomolecularDesign_Fall26/BiomolecularDesign_Fall26App.swift
@@ -0,0 +1,17 @@
+//
+//  BiomolecularDesign_Fall26App.swift
+//  BiomolecularDesign_Fall26
+//
+//  Created by Amogh Reddy Badikol on 9/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BiomolecularDesign_Fall26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
